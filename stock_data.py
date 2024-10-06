b4250e98f3985016b78e12dcde1d49bbee41253f
zedong <chenzedong1126@163.com>
2024-10-06 11:26:48 -0400

    three way for fetch data/ticker/sector/all

--- stock_data.py
+++ stock_data.py
@@ -2,7 +2,13 @@ import requests
 import pandas as pd
 
 class StockData:
+    """
+    A class to fetch and process stock data from the EOD Historical Data API.
+    """
     def __init__(self, csv_file_path: str, api_key: str) -> None:
+        """
+        Initialize the StockData object by loading ticker symbols and sectors from a CSV file.
+        """
         self.ticker_names = pd.read_csv(csv_file_path)['Symbol'].tolist()
         self.ticker_sectors = pd.read_csv(csv_file_path)['GICS Sector'].tolist()
         self.ticker_info = dict(zip(self.ticker_names, self.ticker_sectors))
@@ -10,6 +16,9 @@ class StockData:
         self.base_url = 'https://eodhd.com/api/eod'
 
     def _fetch_data(self, ticker: str, period: str, start: str, end: str):
+        '''
+        private method to fetch stock data for a single ticker symbol from the API.
+        '''
         url = f"{self.base_url}/{ticker}.US?period={period}&from={start}&to={end}&api_token={self.api_key}&fmt=json"
         response = requests.get(url)
         if response.status_code == 200:
@@ -19,14 +28,19 @@ class StockData:
             return None
         
 
-    def fetch_stock(self, ticker: str, period: str, start: str, end: str):
+    def fetch_stock(self, ticker: str, period: str, start: str, end: str) -> pd.DataFrame:
+        """
+        Fetches stock data for a single ticker and returns it as a pandas DataFrame.
+        """
         print(f"fetch data for {ticker}...")
         result = self._fetch_data(ticker, period, start, end)
         df = pd.DataFrame(result)
         return df
     
-    
-    def fetch_all_stocks(self, sector: str, period: str, start: str, end: str):
+    def fetch_stocks_by_sectors(self, sector: str, period: str, start: str, end: str) -> pd.DataFrame:
+        """
+        Fetches stock data for all tickers in a specific GICS sector and returns a merged DataFrame.
+        """
         results = {}
         for ticker, s in self.ticker_info.items():
             if s == sector:
@@ -34,8 +48,22 @@ class StockData:
 
         all_data = self._process_merge_all_data(results)
         return all_data
+    
+    def fetch_all_stocks(self, period: str, start: str, end: str):
+        '''
+        Fetches stock data for all tickers and returns a merged DataFrame.
+        '''
+        results = {}
+        for ticker in self.ticker_info.keys():
+            results[ticker] = pd.DataFrame(self._fetch_data(ticker, period, start, end))
 
-    def _process_merge_all_data(self, dic:dict):
+        all_data = self._process_merge_all_data(results)
+        return all_data
+
+    def _process_merge_all_data(self, dic:dict) -> pd.DataFrame:
+        '''
+        Private method to process and merge stock data from multiple tickers into a single DataFrame.
+        '''
         all_data = []
 
         for name, df in dic.items():
@@ -57,7 +85,8 @@ class StockData:
 if __name__ == "__main__":  
     stock = StockData('sp_400_midcap.csv', '662166cb8e3d13.57537943')
     # df = stock.fetch_stock(ticker = 'AA', period = 'd', start = '2010-01-01', end = '2010-07-01')
-    df = stock.fetch_all_stocks(sector='Materials', period = 'd', start = '2010-01-01', end = '2010-07-01')
+    # df = stock.fetch_stocks_by_sectors(sector='Materials', period = 'd', start = '2010-01-01', end = '2010-07-01')
+    df = stock.fetch_all_stocks(period = 'd', start = '2010-01-01', end = '2010-07-01')
     print(df.head())
     print(df.shape)
         
